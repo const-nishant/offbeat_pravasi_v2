f2638e08cf0f833c20f1550d6bcf4d7ad4ee1d0e
const-nishant <nishantspatil04@gmail.com>
2025-01-24 19:06:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# offbeat_pravasi_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/offbeat_pravasi_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.offbeat_pravasi_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
